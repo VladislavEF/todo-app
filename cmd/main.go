93d65250de8e5e1bbc5261ac2fdc70155f91a70d
VladislavEF <EfimovVladAL@gmail.com>
2021-10-18 20:54:26 +0300

    Добавлено подключение к БД

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/VladislavEF/todo-app"
 	"github.com/VladislavEF/todo-app/pkg/handlers"
 	"github.com/VladislavEF/todo-app/pkg/repository"
 	"github.com/VladislavEF/todo-app/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
-	repos := repository.NewRepository()
+
+	if err := initConfig(); err != nil {
+		log.Fatal("error initialazing configs:", err.Error())
+	}
+
+	if err := gotenv.Load(); err != nil {
+		log.Fatal("error loading env variables:", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatal("failed to initialize database:", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	service := service.NewService(repos)
 	handlers := handlers.NewHandler(service)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running server:", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
