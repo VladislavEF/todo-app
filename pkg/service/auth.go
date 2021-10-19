0082ef6b77407868e48037e10f67b2f70ae2a7ba
VladislavEF <EfimovVladAL@gmail.com>
2021-10-19 11:25:34 +0300

    Реализована аутентификация и генерация JWT токенов

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,15 +3,26 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
 	"github.com/VladislavEF/todo-app"
 	"github.com/VladislavEF/todo-app/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "dgergewtg3453wg3e46y34gh"
+const (
+	salt      = "dgergewtg3453wg3e46y34gh"
+	signigKey = "ftbnysEWGrhgeIVw3etrHIEHhhegasd"
+	tokenTTL  = 12 * time.Hour
+)
 
 type AuthService struct {
 	repo repository.Authorization
+}
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
@@ -21,6 +32,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generateUPasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generateUPasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		UserId: user.Id,
+	})
+
+	return token.SignedString([]byte(signigKey))
 }
 
 func generateUPasswordHash(password string) string {
