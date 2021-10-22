2c0a39d2b1cd24fef879915689c7f0241a3a4f45
VladislavEF <EfimovVladAL@gmail.com>
2021-10-22 17:47:04 +0300

    Добавлено плавное завершение работы сервера

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/VladislavEF/todo-app"
 	"github.com/VladislavEF/todo-app/pkg/handlers"
@@ -42,9 +46,27 @@ func main() {
 	handlers := handlers.NewHandler(service)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatal("error occured while running server:", err.Error())
+
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil && err != http.ErrServerClosed {
+			logrus.Fatal("error occured while running server:", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp started")
+
+	quite := make(chan os.Signal, 1)
+	signal.Notify(quite, syscall.SIGTERM, syscall.SIGINT)
+	<-quite
+
+	logrus.Print("TodoApp shutting down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occurus on server shutting down: %s", err.Error())
 	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occurus on database connection close: %s", err.Error())
+	}
+
 }
 
 func initConfig() error {
